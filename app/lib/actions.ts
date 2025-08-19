86a9ce96c76ae9262a3fe572393c15d6fff74cfc
Aditya kaveti <adityak@elogictech.com>
2025-08-19 16:28:20 +0530

    done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -2,8 +2,9 @@
 import { revalidatePath } from 'next/cache'; 
 import { z } from 'zod';
 import postgres from 'postgres';
-import { redirect } from 'next/navigation';
-
+import { redirect } from 'next/navigation'; 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 export type State = {
   errors?: {
@@ -95,8 +96,25 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
-
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
